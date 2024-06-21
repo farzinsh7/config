ae1f3403f5352e559174b4e16b6bedbe76df998d
farzinsh7 <farzinnater@gmail.com>
2024-06-21 21:18:16 +0330

    First Configuration For Django Project

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account.apps.AccountConfig',
+    
+    # Custom_Apps
 ]
 
 MIDDLEWARE = [
@@ -131,3 +134,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static-cdn', 'media-root')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'account.User'
