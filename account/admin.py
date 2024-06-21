01ccdd63f599447c489f896f2d2a6b3621589494
farzinsh7 <farzinnater@gmail.com>
2024-06-21 21:12:52 +0330

    Create account model & admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
+from .models import User
 
 # Register your models here.
+@admin.register(models.User)
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ['first_name', 'username']
--- account/models.py
+++ account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    # is_author = models.BoolianField(default=False)
+    # special_user = models.DateTimeField(default=timezone.now)
